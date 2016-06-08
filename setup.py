156ba0b616c775513a6201c7c3c1cc05def462d6
Pontus.Skoeldstroem <ponsko@acreo.se>
2016-06-08 14:07:56 +0200

    added future to the requirements

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ setup(name='doubledecker',
       license='LGPLv2.1',
       scripts=['bin/ddclient.py', 'bin/ddkeys.py', ],
       packages=find_packages(),
-      install_requires=['zmq', 'pynacl'])
+      install_requires=['pyzmq', 'pynacl', 'future'])
